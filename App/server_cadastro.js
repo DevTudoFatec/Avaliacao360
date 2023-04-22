d17e5c10ac52b888719224ab060a0dc3f09be10c
João Antonio Saldanha <joaoaosaldanha@gmail.com>
2023-04-22 01:56:03 -0300

    branch cleaning

--- App/server_cadastro.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const data = {};
-const jsonData = JSON.stringify(data);
-
-fs.writeFile('data.json', jsonData, (err) => {
-  if (err) throw err;
-  console.log('Os dados foram salvos em data.json');
-});
