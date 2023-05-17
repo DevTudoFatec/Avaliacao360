53b62e222d2447fd51bd32b1d518958699afe123
Saldanha <joaoaosaldanha@gmail.com>
2023-05-17 20:48:44 -0300

    .

--- API python/main.py
+++ API python/main.py
@@ -5,33 +5,102 @@ import time
 import string
 import random
 import bcrypt
+from functools import wraps
 
 app = Flask(__name__)
 
 ##  DEFININDO ROTA
 
+def login_required(route_function):
+    @wraps(route_function)
+    def decorated_function(*args, **kwargs):
+        if 'email' not in session:
+            return redirect(url_for('home'))
+        return route_function(*args, **kwargs)
+    return decorated_function
+
+def admin_required(route_function):
+    @wraps(route_function)
+    def decorated_function(*args, **kwargs):
+        if session['perfil'] != 2:
+            return redirect(url_for('menu_integrante'))
+        return route_function(*args, **kwargs)
+    return decorated_function
+
 @app.route("/")
 def home():
+  try:  
+    del session['email']
+  except:
+    pass
+
   return render_template('login.html')
 
 @app.route("/menu_admin")
+@login_required
+@admin_required
 def menu_admin():
   return render_template('menu_admin.html', nomeUsuario=session['nomeUsuario'])
 
-@app.route("/menu_integrante")
+@app.route("/menu_integrante", methods=["GET", "POST"])
+@login_required
 def menu_integrante():
-  return render_template('menu_integrante.html', nomeUsuario=session['nomeUsuario'])
+
+  with open("data/cadastro.json", "r") as f:
+    users = json.load(f)
+
+  if "confirm" in request.form:
+    user_time = request.form.get("user_time")
+    for user in users:
+      if user['email'] == session['email']:
+        user['time'] = int(user_time)
+        user['acessos'] += 1
+
+    with open("data/cadastro.json", "w") as file:
+        json.dump(users, file, indent=2)
+  
+  for user in users:
+    if user['email'] == session['email']:
+      if user['acessos'] == 1:
+        primeiro_acesso = True
+      else:
+        primeiro_acesso = False 
+
+  if primeiro_acesso:
+    try:
+      with open("data/times.json", "r") as f:
+        times = json.load(f)
+      
+      times_turma = []
+      for time in times:
+        if time['codigo_turma'] == session['turma']:
+          times_turma.append(time)
+    except:
+      times_turma = []     
+    return render_template('menu_integrante.html', primeiro_acesso=primeiro_acesso, times=times_turma, nomeUsuario=session['nomeUsuario'])
+      
+  else:
+    return render_template('menu_integrante.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/cadastro")
 def cadastro():
   return render_template('cadastro.html')
 
 @app.route("/autoavaliacao")
+@login_required
 def autoavaliacao():
   return render_template('autoavaliacao.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/controle_turmas", methods=["GET", "POST"])
+@login_required
+@admin_required
 def controle_turmas():
+  try:
+      with open("data/cadastro.json", "r") as f:
+        users = json.load(f)
+  except:
+    users=[]
+
   try:
     with open("data/turmas.json", "r") as f:
       turmas = json.load(f)
@@ -51,9 +120,11 @@ def controle_turmas():
   elif "times" in request.form:
     page='times'
 
-  return render_template('controle_turmas.html', turmas=turmas, times=times, page=page, nomeUsuario=session['nomeUsuario'])
+  return render_template('controle_turmas.html', users=users, turmas=turmas, times=times, page=page, nomeUsuario=session['nomeUsuario'])
 
 @app.route("/criar_turma", methods=["POST"])
+@login_required
+@admin_required
 def criar_turma():
   if not os.path.exists('data/turmas.json'):
     if not os.path.exists('data'):
@@ -87,6 +158,8 @@ def criar_turma():
 
 
 @app.route("/criar_time", methods=["POST"])
+@login_required
+@admin_required
 def criar_time():
   if not os.path.exists('data/times.json'):
     if not os.path.exists('data'):
@@ -118,6 +191,8 @@ def criar_time():
 
 
 @app.route("/controle_perfil")
+@login_required
+@admin_required
 def controle_perfil():
   try:
     with open("data/cadastro.json", "r") as f:
@@ -131,6 +206,8 @@ import json
 from flask import request, render_template
 
 @app.route("/update_perfil", methods=["POST"])
+@login_required
+@admin_required
 def update_perfil():
     try:
         with open("data/cadastro.json", "r") as f:
@@ -159,6 +236,8 @@ def update_perfil():
     return render_template('controle_perfil.html', users=users, editing=editing, index=index)
 
 @app.route("/update_geral", methods=["POST"])
+@login_required
+@admin_required
 def update_geral():
     try:
         with open("data/cadastro.json", "r") as f:
@@ -213,6 +292,8 @@ def update_geral():
     return render_template('controle_geral.html', users=users, turmas=turmas, times=times, editing=editing, index=index)
 
 @app.route("/controle_geral")
+@login_required
+@admin_required
 def controle_geral():
   try:
     with open("data/cadastro.json", "r") as f:
@@ -237,10 +318,12 @@ def controle_geral():
   return render_template('controle_geral.html', times=times, turmas=turmas, users=users, nomeUsuario=session['nomeUsuario'])
 
 @app.route("/pre_devolutiva")
+@login_required
 def pre_devolutiva():
   return render_template('pre_devolutiva_avaliacao.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/pre_devolutiva_submit", methods=["POST"])
+@login_required
 def pre_devolutiva_submit():
   sprint = request.form.get('sprint')
 
@@ -283,6 +366,8 @@ def pre_devolutiva_submit():
 
 
 @app.route("/pre_devolutiva_submit_admin", methods=["POST"])
+@login_required
+@admin_required
 def pre_devolutiva_submit_admin():
   sprint = request.form.get('sprint')
   integrante = request.form.get('integrante')
@@ -336,10 +421,14 @@ def pre_devolutiva_submit_admin():
   return render_template('devolutiva_admin.html', nomeUsuario=session['nomeUsuario'], sprint=sprint, int_infos = int_infos, integrante=integrante, avgs=avgs, avaliacoes=avaliacoes, autoavaliacoes=autoavaliacoes, email=session['email'])
 
 @app.route("/devolutiva_admin")
+@login_required
+@admin_required
 def devolutiva_admin():
   return render_template('devolutiva_admin.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/pre_devolutiva_admin")
+@login_required
+@admin_required
 def pre_devolutiva_admin():
   try:
     with open("data/cadastro.json", "r") as f:
@@ -350,14 +439,19 @@ def pre_devolutiva_admin():
   return render_template('pre_devolutiva_admin.html', users=users, nomeUsuario=session['nomeUsuario'])
 
 @app.route("/dashboards_gerenciais")
+@login_required
+@admin_required
 def dashboards_gerenciais():
   return render_template('dashboards_gerenciais.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/dashboards_operacionais")
+@login_required
+@admin_required
 def dashboards_operacionais():
   return render_template('dashboards_operacionais.html', nomeUsuario=session['nomeUsuario'])
 
 @app.route("/avaliacao")
+@login_required
 def avaliacao():
   try:
     with open("data/cadastro.json", "r") as f:
@@ -382,6 +476,11 @@ def login():
         check = True
         session['nomeUsuario'] = item['nome']
         session['email'] = item['email']
+        session['perfil'] = item['perfil']
+        session['turma'] = item['turma']
+        item['acessos'] += 1
+        with open("data/cadastro.json", "w") as file:
+            json.dump(data, file, indent=2)
         if item['perfil'] == 2:
           return redirect(url_for('menu_admin'))
         else:
@@ -389,12 +488,7 @@ def login():
   if not check:
     flash('Usuário ou Senha inválidos')
     return redirect('/')
-    
 
-## função para geração de senha aleatoria do usuario
-
-def senha_aleatoria(tamanho, caracteres):
-    return ''.join(random.choice(caracteres) for _ in range(tamanho))
 
 ### ARMAZENA DADOS DE CADASTRO EM UM JSON
 
@@ -423,6 +517,18 @@ def cadastro_submit():
     flash('Email já cadastrado!')
     return redirect(url_for('cadastro'))
 
+  check_turma = False
+
+  with open('data/turmas.json', 'r') as f:
+    turmas = json.load(f)
+  for turma in turmas:
+    if codigo_turma == turma['codigo'] :
+      check_turma = True
+
+  if not check_turma:
+    flash('Codigo de turma não existente!')
+    return redirect(url_for('cadastro'))
+
   cadastro_dict = {
     "index": len(data),
     "nome": nome.title(),
@@ -430,7 +536,8 @@ def cadastro_submit():
     "turma": codigo_turma,
     "time": 0,
     "senha": hashed_password.decode('utf-8'),
-    "perfil": 1
+    "perfil": 1,
+    "acessos": 0
   }
 
   data.append(cadastro_dict)
@@ -443,6 +550,7 @@ def cadastro_submit():
 
 
 @app.route("/autoavaliacao_submit", methods=["POST"])
+@login_required
 def autoavaliacao_submit():
 
   if not os.path.exists('data/autoavaliacao.json'):
@@ -487,6 +595,7 @@ def autoavaliacao_submit():
   return redirect(url_for('autoavaliacao'))
 
 @app.route("/avaliacao_submit", methods=["POST"])
+@login_required
 def avaliacao_submit():
 
   if not os.path.exists('data/avaliacao.json'):
