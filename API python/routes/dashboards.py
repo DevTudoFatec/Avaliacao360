8ce999c16741d887aaf6dd841f81195bbd31598c
BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
2023-06-04 01:25:35 -0300

    grafico de aderencia geral

--- API python/routes/dashboards.py
+++ API python/routes/dashboards.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, session, Blueprint as bp
 import json
 from utils.decorators import login_required, admin_required
-import plotly.graph_objects as go
+import plotly.express as px
 from dash import Dash, html, dcc
 import pandas as pd
 
@@ -17,56 +17,69 @@ def dashboards_operacionais():
 @admin_required
 @bp.route("/dashboards_gerenciais")
 def dashboards_gerenciais():
-    with open("data/avaliacao.json", "r") as file:
-        data = json.load(file)
-        df = pd.DataFrame(data)
 
-    def calculo_sprint(competencia):
-        soma_comu = 0 
-        qtd_comu = 0
+    df = pd.read_json('data/cadastro.json')
+    aderencia_avaliacoes = df['count_avaliacao'].value_counts()
 
-        for i in data:
-            soma_comu += i[competencia] 
-            qtd_comu += 1
-        
-        return (soma_comu / qtd_comu)
+    fig = px.pie(aderencia_avaliacoes, names=aderencia_avaliacoes.index, values=aderencia_avaliacoes.values)
+    figure = px.bar(df, x='turma', y='count_avaliacao', barmode='group', color='time')
 
-    fig = go.Figure()
-
-    fig.add_trace(go.Bar(
-        x=['sprint'],
-        y=[calculo_sprint('comunicacao')]
-    ))
-
-    fig.update_layout(
-        autosize=False,
-        width=500,
-        height=500,
-        yaxis=dict(
-            title_text="Média das notas",
-            tickvals=[1, 2, 3, 4],
-            tickmode="array",
-            titlefont=dict(size=30),
-        )
-    )
-    fig.add_trace(go.Bar(
-            x=['sprint'],
-            y=[calculo_sprint('engajamento')]
-        ))
-
-    fig.update_layout(
-        autosize=False,
-        width=500,
-        height=500,
-        yaxis=dict(
-            tickvals=[1, 2, 3, 4],
-            tickmode="array",
-            titlefont=dict(size=30),
-            )
-        )
-    fig.update_yaxes(automargin=True)
-
-    
     div_grafico = fig.to_html(full_html=False)
+    div_grafico_sprint = figure.to_html(full_html=False)
 
     return render_template('admin/dashboards_gerenciais.html', nomeUsuario=session['nomeUsuario'], darkmode=session['darkmode'],div_grafico=div_grafico)
+
+
+    # with open("data/avaliacao.json", "r") as file:
+    #     data = json.load(file)
+    #     df = pd.DataFrame(data)
+
+    # def calculo_sprint(competencia):
+    #     soma_comu = 0 
+    #     qtd_comu = 0
+
+    #     for i in data:
+    #         soma_comu += i[competencia] 
+    #         qtd_comu += 1
+        
+    #     return (soma_comu / qtd_comu)
+
+    # fig = go.Figure()
+
+    # fig.add_trace(go.Bar(
+    #     x=['sprint'],
+    #     y=[calculo_sprint('comunicacao')]
+    # ))
+
+    # fig.update_layout(
+    #     autosize=False,
+    #     width=500,
+    #     height=500,
+    #     yaxis=dict(
+    #         title_text="Média das notas",
+    #         tickvals=[1, 2, 3, 4],
+    #         tickmode="array",
+    #         titlefont=dict(size=30),
+    #     )
+    # )
+    # fig.add_trace(go.Bar(
+    #         x=['sprint'],
+    #         y=[calculo_sprint('engajamento')]
+    #     ))
+
+    # fig.update_layout(
+    #     autosize=False,
+    #     width=500,
+    #     height=500,
+    #     yaxis=dict(
+    #         tickvals=[1, 2, 3, 4],
+    #         tickmode="array",
+    #         titlefont=dict(size=30),
+    #         )
+    #     )
+    # fig.update_yaxes(automargin=True)
+
+    
+    # div_grafico = fig.to_html(full_html=False)
+
+    # return render_template('admin/dashboards_gerenciais.html', nomeUsuario=session['nomeUsuario'], darkmode=session['darkmode'],div_grafico=div_grafico)
