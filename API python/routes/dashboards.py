57b19cb8114b3ba0b68d35b35d23cddc96e81667
BeatrizPlacido <127264395+BeatrizPlacido@users.noreply.github.com>
2023-06-04 18:35:52 -0300

    Atualizações na página do dashboard gerencial

--- API python/routes/dashboards.py
+++ API python/routes/dashboards.py
@@ -2,8 +2,6 @@ from flask import render_template, request, redirect, url_for, session, Blueprin
 import json
 from utils.decorators import login_required, admin_required
 import plotly.express as px
-from dash import Dash, html, dcc
-from dash.dependencies import Input, Output
 import pandas as pd
 
 bp = bp('dashboards', __name__)
@@ -12,15 +10,29 @@ bp = bp('dashboards', __name__)
 @admin_required
 @bp.route("/dashboards_gerenciais", methods=['GET', 'POST'])
 def dashboards_gerenciais():
+ # Gráfico geral
     df = pd.read_json('data/cadastro.json')
-    aderencia_avaliacoes = df['count_avaliacao'].value_counts()
+    aderencia_avaliacoes_geral = df['count_avaliacao'].value_counts()
+    aderencia_avaliacoes_geral.index = ['OK', 'Pendente']  # Renomeando os índices
+    fig = px.pie(aderencia_avaliacoes_geral, names=aderencia_avaliacoes_geral.index, values=aderencia_avaliacoes_geral.values, color_discrete_sequence=['#41B8D5', '#6CE5E8'])
 
-    fig = px.pie(aderencia_avaliacoes, names=aderencia_avaliacoes.index, values=aderencia_avaliacoes.values)
-    figure = px.bar(df, x='turma', y='count_avaliacao', barmode='group', color='time')
+    #Gráfico por sprints
+    df['avaliacoes'] = df['avaliacoes'].apply(json.dumps)
+
+    df_respondidas = df[df['count_avaliacao'] == 1]
+    df_pendentes = df[df['count_avaliacao'] == 0]
+
+    df_respondidas['status'] = 'OK'
+    df_pendentes['status'] = 'Pendente'
+
+    df_combined = pd.concat([df_respondidas, df_pendentes])
+
+    figure = px.bar(df_combined, x='avaliacoes', color='status', barmode='group', color_discrete_sequence=['#41B8D5', '#6CE5E8'])
+
 
     div_grafico = fig.to_html(full_html=False)
     div_grafico_sprint = figure.to_html(full_html=False)
 
-    return render_template('admin/dashboards_gerenciais.html', nomeUsuario=session['nomeUsuario'], darkmode=session['darkmode'],div_grafico=div_grafico)
+    return render_template('admin/dashboards_gerenciais.html', nomeUsuario=session['nomeUsuario'], darkmode=session['darkmode'],div_grafico=div_grafico, div_grafico_sprint=div_grafico_sprint)
 
 
